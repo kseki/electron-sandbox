48d1fa59b200db420e71ce5c115d57332c761757
kota718 <kota.yt@gmail.com>
2018-09-04 23:42:33 +0900

    ファイル移動

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const {app, BrowserWindow} = require('electron');
-const path = require('path');
-const url = require("url");
 let win;
 
 const createWindow = () => {
   win = new BrowserWindow({width: 800, height: 600});
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file',
-    slashes: true,
-  }));
+  win.loadFile('index.html');
+
+  if (process.argv.find((arg) => arg === '--debug')) {
+    win.webContents.openDevTools();
+  }
 
   win.on('closed', () => {
     win = null;
